5c6043e654d2bd2b47f70090fddb9d14ef41d497
DarkClouds <olga.knaub@gmx.net>
2011-05-15 15:56:10 +0200

    Befüllung der Standorte

--- js/MixAndMatch.js
+++ js/MixAndMatch.js
@@ -2,37 +2,29 @@
  * This is the main MixAndMatch class.
  */
 function MixAndMatch(pRemoteService) {
-    
+
     /** private property for logging */
     var log = log4javascript.getLogger("de.html5.MixAndMatch");
-    
+
     /** the remote service for ajax calls */
     var remoteService = pRemoteService;
-    
+
     /**
      * public checkin function
      */
-    this.getLocations = function() {
-        log.debug('checkin()');
-        remoteService.getLocations(getLocationsCallback);
+    this.getLocations = function(callback) {
+        log.debug('getLocations');
+        remoteService.getLocations(callback);
     }
-    
-    /**
-     * callback function for the getLocations method
-     */
-    function getLocationsCallback(data) {
-        log.debug('getLocationsCallback');
-        log.debug('list object:', JSON.stringify(data));
-    }
-    
+
     /**
      * public lunch response function
      */
     this.getLunchResponse = function(callback, userDataObject) {
-    	log.debug('lunch response');
-    	remoteService.getLunchResponse(callback, userDataObject);
+        log.debug('lunch response');
+        remoteService.getLunchResponse(callback, userDataObject);
     }
-    
+
     /** use the private log object */
     log.debug('MixAndMatch created');
 }
--- js/RemoteService.js
+++ js/RemoteService.js
@@ -2,39 +2,37 @@
  * Dummy implementation for the remote service.
  */
 function RemoteService() {
-    
+
     /** private property for logging */
     var log = log4javascript.getLogger('de.html5.RemoteServiceDummy');
-    
+
     /**
      * call the backend to get a list of available locations
      */
     this.getLocations = function(pCallback) {
         log.debug('getLocations()');
-        
+
         // jQuery Ajax call with an anonymous callback function
         $.getJSON('http://mixmatch-t.elasticbeanstalk.com/locations?callback=?', function(data) {
             log.debug("Location data:", JSON.stringify(data));
-            // write the data as string to the view
-            $('#checkinResponse').text(JSON.stringify(data));
             // execute the callback parameter to get back to the MixAndMatch object
             pCallback(data);
         });
     }
-    
+
     /**
      * call the backend to get a list of all lunch responses
      */
     this.getLunchResponse = function(pCallback, userRequestObject) {
-    	var url = 'http://mixmatch-t.elasticbeanstalk.com/requests?callback=?'
-    	
-    	$.getJSON(url, function(data) {
-             log.debug("Response data:", JSON.stringify(data));
-             // execute the callback parameter to get back to the Response object
-             pCallback(data);
+        var url = 'http://mixmatch-t.elasticbeanstalk.com/requests?callback=?'
+
+        $.getJSON(url, function(data) {
+            log.debug("Response data:", JSON.stringify(data));
+            // execute the callback parameter to get back to the Response object
+            pCallback(data);
         });
     }
-    
+
     /** use the private log object */
     log.debug('RemoteServiceDummy created');
 }
