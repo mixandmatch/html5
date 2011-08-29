cb37fcab9effe9d35bd4aec051479f7aacf35fe7
Benedikt Arnold <benedikt.arnold@gmx.net>
2011-08-29 19:57:50 +0200

    Alle Verabredungen wird nun mit /matches Endpoint implementiert. Dieser Endpoint liefert pro location und Tag nur eine sog. Match mit allen Teilnehmern.

--- js/CommonHeaderInit.js
+++ js/CommonHeaderInit.js
@@ -15,8 +15,8 @@ var Utils = new UtilsClass();
 $(document).ready(function(){
 	
 	// create the remote service object which should be used in MixAndMatch
-	var remoteService = new RemoteService('http://ec2-46-137-12-115.eu-west-1.compute.amazonaws.com/api');
-	//var remoteService = new RemoteService('http://192.168.99.23:8080');
+	//var remoteService = new RemoteService('http://ec2-46-137-12-115.eu-west-1.compute.amazonaws.com/api');
+	var remoteService = new RemoteService('http://localhost:8080/spring-backend');
 
 	/*
 	var remoteServiceValidator = new RemoteServiceValidator();
